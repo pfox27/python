7fe318069a783928512ab09ba75a7b62017d45fa
pfox27 <pfox27@gmail.com>
2024-02-25 12:39:02 -0500

    update GeoClac.py

--- GeoCalc.py
+++ GeoCalc.py
@@ -41,3 +41,22 @@ if shape == 2:
         radius = int(input ('Please enter the radius of the sphere in metres:'))
         volume = 4/3 * math.pi * radius**3
         print ('The volume of the sphere is ', volume, ' m^3')
+
+if shape == 3:
+    print ('Please enter the type of calculation desired:')
+    print ('1. Area')
+    print ('2. Volume')
+    calc = int(input())
+    if calc == 1:
+        h = int(input ('Please enter the height of the rectangular pyramid in metres:'))
+        w = int(input ('Please enter the width of the base of the rectangular pyramid in metres:'))
+        l = int(input ('Please enter the length of the base of the rectangular pyramid in metres:'))
+
+        area = l * w + w*math.sqrt(h**2 + l**2/4) + l*math.sqrt(h**2 + w**2/4) 
+        print ('The surface area of the rectangular prism is ', area, ' m^2')
+    else:
+        radius = int(input ('Please enter the radius of the sphere in metres:'))
+        volume = 4/3 * math.pi * radius**3
+        print ('The volume of the sphere is ', volume, ' m^3')
+
+
