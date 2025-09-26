51e1eeb3a548def1ac3f86571b27cd22918a0cbb
pfox27 <80210760+pfox27@users.noreply.github.com>
2025-09-26 10:19:36 -0400

    update

---
+++ U1/U1L1Activity1.py
@@ -0,0 +1,7 @@
+# This programme asks the user to input a number that is then multiplied by 9.  The result is printed to the screen. 
+
+
+number = int(input("Please enter a number:"))
+print(number * 9)
+
+	
---
+++ U1/U1_SillySentences.py
@@ -0,0 +1,36 @@
+# This programme asks the user to input several words and then outputs a sentence (from a template) using those words. 
+
+
+name = input("Please enter a name:")
+adjective1 = input("Please enter an adjective:")
+adjective2 = input("Please enter another adjective:")
+adverb1 = input("Please enter an adverb:")
+food1 = input("Please enter a food:")
+food2 = input("Please enter another food:")
+noun = input("Please enter a noun:")
+place = input("Please enter a place:")
+verb = input("Please enter a verb:")
+
+print(name + " was planning a dream vacation to Paris." + name + " was especially looking forward to trying cuisine, including " + adjective1 + food1 + " and " + food2 + "." + name + " will have to practice the language " + adverb + " to make it easier to " + verb + " with people." + name + " has a long list of sights to see, including the " + noun + " museum and the " + adjective2 + place + ".")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
