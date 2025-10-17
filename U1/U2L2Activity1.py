87375c069a49e8c8957fb1fd6956e26bc11c79ec
pfox27 <80210760+pfox27@users.noreply.github.com>
2025-10-17 13:33:28 -0400

    update

---
+++ U1/U2L2Activity1.py
@@ -0,0 +1,11 @@
+# This programme asks the user to input an integer and then outputs the three preceding integers in descending order on separate lines. 
+
+
+number = int(input("Please enter a number: "))
+print(number - 1)
+print(number - 2)
+print(number - 3)
+
+
+
+	
---
+++ U1/U2L2Activity2.py
@@ -0,0 +1,10 @@
+# This programme asks the user to input a decimal number and then outputs their sum. 
+
+
+number1 = float(input("Please enter a decimal number: "))
+number2 = float(input("Please enter another decimal number: "))
+print(number1 + number2)
+
+
+
+	
